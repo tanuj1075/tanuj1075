3329e9eab943853b93999ff1e3147984c5cbeed1
tanuj1075 <tanujdorugade1075@gmail.com>
2026-04-14 23:50:40 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -1,47 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+from mlxtend.frequent_patterns import apriori
+from mlxtend.preprocessing import TransactionEncoder
+import pandas as pd
 
-int main() {
-    int req[] = {176,79,34,60,92,11,41,114};
-    int n = 8;
-    int head = 65;
-    int total = 0;
+data=[['eggs','milk','bread'],['eggs','apple'],
+      ['milk','bread'],['apple','milk'],
+      ['milk','apple','bread']]
 
-    // Sort requests
-    for(int i=0;i<n-1;i++) {
-        for(int j=0;j<n-i-1;j++) {
-            if(req[j] > req[j+1]) {
-                int temp = req[j];
-                req[j] = req[j+1];
-                req[j+1] = temp;
-            }
-        }
-    }
+te=TransactionEncoder()
+df=pd.DataFrame(te.fit(data).transform(data),columns=te.columns_)
 
-    printf("Order of service:\n");
-
-    // Move LEFT
-    for(int i=n-1;i>=0;i--) {
-        if(req[i] < head) {
-            printf("%d -> ", req[i]);
-            total += abs(head - req[i]);
-            head = req[i];
-        }
-    }
-
-    // Go to 0 (end)
-    total += head;
-    head = 0;
-
-    // Move RIGHT
-    for(int i=0;i<n;i++) {
-        if(req[i] > head) {
-            printf("%d -> ", req[i]);
-            total += abs(head - req[i]);
-            head = req[i];
-        }
-    }
-
-    printf("\nTotal Head Movement = %d\n", total);
-    return 0;
-}
+print(apriori(df,min_support=0.4,use_colnames=True))
